e5d04349f648d478df2ec368e9fc9498b053a51a
Rohit <rohitmpaul@gmail.com>
2019-10-26 11:00:31 +0530

    Add Chat and Login

---
+++ src/screens/Chat.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+
+export class Chat extends Component {
+  render() {
+    return <GiftedChat />;
+  }
+}
+export default Chat;
---
+++ src/screens/Login.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {Text, View, Button, Alert, TextInput} from 'react-native';
+//import AsyncStorage from '@react-native-community/async-storage';
+
+import firebaseSvc from '../FirebaseSvc';
+
+export class Login extends Component {
+  state = {
+    email: 'testA@gmail.com',
+    password: 'testapass',
+  };
+
+  // add login method to handle user press Login button
+  onPressLogin = async () => {
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    firebaseSvc.login(user, this.loginSuccess, this.loginFailed);
+  };
+
+  loginSuccess = () => {
+    console.log('login successful, navigate to chat.');
+    this.props.navigation.navigate('ChatScreen', {
+      name: this.state.name,
+      email: this.state.email,
+    });
+  };
+
+  loginFailed = () => {
+    Alert.alert('Login failure. Please try again.');
+  };
+
+  // methods to handle user input and update the state
+  onChangeTextEmail = email => this.setState({email});
+  onChangeTextPassword = password => this.setState({password});
+
+  render() {
+    return (
+      <View>
+        <Text>My Login Screen</Text>
+        <Text>Username</Text>
+        <TextInput
+          value={this.state.email}
+          onChangeText={this.onChangeTextEmail}
+        />
+        <Text>Password</Text>
+        <TextInput
+          value={this.state.password}
+          onChangeText={this.onChangeTextPassword}
+        />
+        <Button title="Login" onPress={this.onPressLogin} />
+      </View>
+    );
+  }
+}
+
+export default Login;

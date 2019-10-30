95834393887b7f59be3b1ddb9a6d425a09f8d6e2
Rohit <rohitmpaul@gmail.com>
2019-10-31 03:01:59 +0530

    Add Messages

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,9 +1,40 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
+import firebaseSvc from '../../FirebaseSvc';
 
-export class Chat extends Component {
+export default class Chat extends React.Component {
+  state = {
+    messages: [],
+  };
+
   render() {
-    return <GiftedChat />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={message => {
+          firebaseSvc.sendMessage(message);
+        }}
+        user={{
+          _id: firebaseSvc.getUid(),
+          name: this.props.username,
+        }}
+      />
+    );
+  }
+  componentDidMount() {
+    firebaseSvc.loadMessages(message => {
+      this.setState(previousState => {
+        return {
+          messages: GiftedChat.append(previousState.messages, message),
+        };
+      });
+    });
+  }
+  componentWillUnmount() {
+    firebaseSvc.closeChat();
   }
 }
-export default Chat;
+
+Chat.defaultProps = {
+  username: 'Default Name',
+};

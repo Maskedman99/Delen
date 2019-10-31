284d7a64c92bd23c821f427300e036165f05d824
Rohit <rohitmpaul@gmail.com>
2019-10-31 18:18:54 +0530

    move firebase congigurations to a seperate directory

--- FirebaseSvc.js
+++ FirebaseSvc.js
@@ -1,25 +1,99 @@
 import firebase from 'firebase';
+import {Alert} from 'react-native';
+
+import {firebaseConfig} from './src/config/firsbaseconfigs';
 
 class FirebaseSvc {
+  uid = '';
+  messagesRef = null;
   constructor() {
     if (!firebase.apps.length) {
       //avoid re-initializing
-      firebase.initializeApp({
-        apiKey: '<your-api-key>',
-        authDomain: '<your-auth-domain>',
-        databaseURL: 'https://<your-db-url>.firebaseio.com',
-        projectId: '<your-project-id>',
-        storageBucket: '<your-storage-bucket>.appspot.com',
-        messagingSenderId: '<your-sender-id>',
-      });
+      firebase.initializeApp(firebaseConfig);
     }
   }
+
   login = async (user, success_callback, failed_callback) => {
     await firebase
       .auth()
       .signInWithEmailAndPassword(user.email, user.password)
       .then(success_callback, failed_callback);
   };
+
+  createAccount = async user => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(user.email, user.password)
+      .then(
+        function() {
+          console.log(
+            'created user successfully. User email:' +
+              user.email +
+              ' name:' +
+              user.name,
+          );
+          var userf = firebase.auth().currentUser;
+          userf.updateProfile({displayName: user.name}).then(
+            function() {
+              Alert.alert(
+                'User ' +
+                  user.name +
+                  ' was created successfully. Please login.',
+              );
+            },
+            function(error) {
+              console.warn('Error update displayName.');
+            },
+          );
+        },
+        function(error) {
+          Alert.alert(error.message);
+        },
+      );
+  };
+
+  //   =================================== MESSAGES =======================================
+
+  setUid(value) {
+    this.uid = value;
+  }
+  getUid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+  // retrieve the messages from the Backend
+  loadMessages(callback) {
+    this.messagesRef = firebase.database().ref('messages');
+    this.messagesRef.off();
+    const onReceive = data => {
+      const message = data.val();
+      callback({
+        _id: data.key,
+        text: message.text,
+        createdAt: new Date(message.createdAt),
+        user: {
+          _id: message.user._id,
+          name: message.user.name,
+        },
+      });
+    };
+    this.messagesRef.limitToLast(20).on('child_added', onReceive);
+  }
+  // send the message to the Backend
+  sendMessage(message) {
+    for (let i = 0; i < message.length; i++) {
+      this.messagesRef.push({
+        text: message[i].text,
+        user: message[i].user,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+      });
+    }
+  }
+  // close the connection to the Backend
+  closeChat() {
+    if (this.messagesRef) {
+      this.messagesRef.off();
+    }
+  }
 }
 
 const firebaseSvc = new FirebaseSvc();

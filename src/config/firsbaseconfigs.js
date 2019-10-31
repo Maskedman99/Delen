2e9e952d4b259c273f023448116b13378d13f7b9
Rohit <rohitmpaul@gmail.com>
2019-10-31 18:15:46 +0530

    move firebase configuration to a seperate directory

---
+++ src/config/firsbaseconfigs.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: 'https://<your-db-url>.firebaseio.com',
+  projectId: '<your-project-id>',
+  storageBucket: '<your-storage-bucket>.appspot.com',
+  messagingSenderId: '<your-sender-id>',
+};
